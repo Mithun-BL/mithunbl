459b0c4e8f04e157845c36d5e05e8400c6fe7b6d
Mithun BL <76171495+Mithun-BL@users.noreply.github.com>
2024-10-23 16:28:43 +0530

    Initial-setup-and-banner-component

---
+++ mithun-portfolio/src/component/HomePage/Banner/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+import classNames from 'classnames';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../Banner/HomeBanner.module.scss';
+
+const BannerData = 
+  {
+    Greetings:"Hi I'm Mithun 👋",
+    Title:'A passionate web developer who loves to build Creative as well as Interactive websites',  
+    Links : [
+      {Text:'My Work', TextUrl:'https://paicon.com/'},
+      {Text:'About Me', TextUrl:'https://terra-cms.irepo.in/myntra_life/'}
+    ]
+  }
+
+
+const HomeBanner = () => {
+
+  return (
+    <div>
+      <section className={classNames(styles.banner, 'my-5')}>
+        <Container>
+          <div className={styles.bannerContent}>
+            <span>{BannerData.Greetings}</span>
+            <h2>{BannerData.Title}</h2>
+            <div>
+              {BannerData.Links.map((Link, index) => (
+                <a key={index} href={Link.TextUrl} className={classNames('text-decoration-none', index === 0 ? 'btn_black me-3' : 'btn_bdr')}>
+                  {Link.Text}
+                </a>
+               ))}
+              {/* <a href="#" className='text-decoration-none btn_bdr'>{BannerData.AboutLinkText}</a> */}
+            </div>    
+          </div>    
+        </Container>
+      </section>
+    </div>
+  )
+}
+
+export default HomeBanner
---
+++ mithun-portfolio/src/component/HomePage/ProjectsHandled/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectsHandled = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default ProjectsHandled
